ea59af13955f0c74519adc1867cbd6e703674780
Eric Murphy <murphye@gmail.com>
2023-08-29 10:13:03 -0700

    Update setup.sql

--- hasura-demo/setup.sql
+++ hasura-demo/setup.sql
@@ -24,18 +24,6 @@ CREATE TABLE article_tag (
   CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tag(id)
 );
 
-CREATE VIEW article_author_tag AS 
-  SELECT 
-  art.id as article_id,
-  art.title,
-  art.content,
-  auth.id as author_id,
-  auth.name,
-  array(
-    SELECT tag.tag_value
-    FROM tag 
-    JOIN article_tag art_tag ON art_tag.article_id = art.id
-    WHERE art_tag.tag_id = tag.id
-   ) AS tag_values
-  FROM article art 
-  JOIN author auth ON auth.id = art.author_id
+
+
+
